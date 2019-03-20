5daad94b95ef5f9ddae0a26d753f20768012382a
Allen <allen@webriq.services>
2019-03-20 17:12:47 +0800

    update layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default () => (
   <footer className="bg-white pt-4">
         <div className="container py-5">
             <div className="row mb-5">
-                <div className="col-md-5 pr-3 pr-md-5">
+                <div className="col-md-4">
                     <h4 className="mb-4">Why Uganda?</h4>
                     <p>Life is hard in Uganda, thanks to economic instability and political corruption most people live in poverty and must grow their own food to survive. But much like their coffee, the people are beautiful and full of surprises; needing
                         a hand-up more than a hand-out. Proceeds from every bag of Eagle &amp; Crane Coffee go to development and education efforts in this remarkable East African Country.</p>
@@ -23,7 +23,7 @@ export default () => (
                         <li><Link to="/">Contact Us</Link></li>
                     </ul>
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-3">
                     <h4 className="mb-4">Company</h4>
                     <ul className="list-unstyled ml-0 footer-info">
                         <li><LazyLoadImage effect="blur" src="/img/001-coffee-cup.svg" width="20" className="mr-2 mb-0" alt="business" /><span>Eagle &amp; Crane Coffee Co</span></li>
@@ -31,7 +31,13 @@ export default () => (
                         <li><LazyLoadImage effect="blur" src="/img/003-telephone.svg" width="20" className="mr-2 mb-0" alt="phone" /><a href="tel://6159006871">615-900-6871</a></li>
                         <li><LazyLoadImage effect="blur" src="/img/004-mail.svg" width="20" className="mr-2 mb-0" alt="email" /><a href="mailto:info@eagleandcrane.com">info@eagleandcrane.com</a></li>
                     </ul>
-                    <a href="https://www.facebook.com/eagleandcrane" target="_blank" rel="noopener noreferrer"><LazyLoadImage effect="blur" className="mb-0 mr-2" src="/img/002-facebook.svg" width="40" alt="facebook" /></a>
+                </div>
+                <div className="col-md-3">
+                  <h4 className="mb-4">Operating Hours</h4>
+                  <p>Weekdays (9am - 5pm)</p>
+                  <p>Weekends (Closed)</p>
+                  <h5>Social</h5>
+                  <a href="https://www.facebook.com/eagleandcrane" target="_blank" rel="noopener noreferrer"><LazyLoadImage effect="blur" className="mb-0 mr-2" src="/img/002-facebook.svg" width="40" alt="facebook" /></a>
                     <a href="https://www.instagram.com/eaglecranecoffee/" target="_blank" rel="noopener noreferrer"><LazyLoadImage effect="blur" className="mb-0 mr-2" src="/img/001-instagram.svg" width="40" alt="instagram" /></a>
                     <a href="https://vimeo.com/user25548849" target="_blank" rel="noopener noreferrer"><LazyLoadImage effect="blur" className="mb-0 mr-2" src="/img/002-vimeo.svg" width="40" alt="instagram" /></a>
                 </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,7 @@
 import React from 'react'
 // import { Link } from 'gatsby'
 import './hero.css'
-import Modal from './Modal'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { Link } from "react-scroll";
 
 
 class Hero extends React.Component {
@@ -20,29 +19,22 @@ class Hero extends React.Component {
             <iframe title="video" src="https://player.vimeo.com/video/295221419?background=1&autoplay=1&loop=1&byline=0&title=0&muted=1" frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe></div>
         <div className="container">
             <div className="row">
-                <div className="col-md-8 pr-md-5">
-                    <div className="hero-content">
-                        <h1 className="hero-title text-white">LIKE FREE COFFEE?</h1>
-                        <h4 className="hero-subtitle text-white">Join the Coffee Club and start with a free bag! Just pay $5 for shipping.</h4>
-                        <div className="d-flex justify-content-start mt-4">
-                            <AnchorLink href='#subscribe' className="alink hvr-shadow mr-3">Subscribe</AnchorLink>
-                            <Modal
-                                size='lg'
-                                id='play-button'
-                                html=<img src="/img/play-arrow.svg" width="30" className="mb-0" alt="play"/>
-                                buttonclass='btn btn-primary playlink'
-                                >
-                                <div style={{padding:`56.25% 0 0 0`, position:`relative`}}>
-                                    <iframe title="eagle" id="myvideo" src="https://player.vimeo.com/video/295221419?autoplay=1&title=0&byline=0&portrait=0&loop=1" 
-                                        style={{
-                                            position:`absolute`,
-                                            top:0,
-                                            left:0,
-                                            width:`100%`,
-                                            height:`100%`
-                                        }} allow="autoplay" frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe>
-                                </div>
-                            </Modal>
+                <div className="col-12">
+                    <div className="hero-content text-center">
+                        <h1 className="hero-title text-white">EAGLE & CRANE COFFEE CO.</h1>
+                        <h4 className="hero-subtitle text-white">COFFEE // SUBSCRIPTION // DIRECT TRADE</h4>
+                        <div className="d-flex justify-content-center mt-5">
+                            <Link
+                                activeClass="active"
+                                to="subscription"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration= {500}
+                                className="alink hvr-shadow mr-3"
+                            >
+                            Subscribe
+                            </Link>
                         </div>
                     </div>
                 </div>
