99f7759aad0ccbd6c635a5bd1919fab9dc214cba
Allen <allen@webriq.services>
2019-04-10 16:12:01 +0800

    fix partner dropdown

--- src/pages/partner.js
+++ src/pages/partner.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react'
 import { graphql } from 'gatsby'
 import { Container, Form, Button, Col, Row } from 'react-bootstrap'
-import { LazyLoadComponent } from 'react-lazy-load-image-component'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
 class PartnerPage extends React.Component {
-  loadScript() {
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.id = 'webriqform'
-    script.src = 'https://forms.webriq.com/js/initForms'
-    document.body.appendChild(script)
-    const headScript = document.getElementsByTagName('script')[0]
-    headScript.parentNode.insertBefore(script, headScript)
-  }
-  componentWillUnmount() {
-    if(window) {
-      console.log('remove script')
-      document.getElementById('webriqform').remove();
-    }
-  }
-  componentDidMount(){
-    if(window) {
-     this.loadScript();
-    }
-  }
+
   constructor(props) {
     super(props);
     this.state = {
       show: false,
-      value: '' || null
+      value: ""
     };
 
   }
@@ -59,7 +39,6 @@ class PartnerPage extends React.Component {
               <Col md={7}>
                 <h5 className="mb-5">Fill in the form below to subscribe.</h5>
                 <div id="partner-subcribe">
-                    <LazyLoadComponent delayTime={500}>
                     <Form name="Partners Subscription"  method="POST" data-form-id="5caae1e4e624d64a2848d088" webriq="true" className="form-partner">
                       <div className="form-row">
                         <div className="col-12 mb-3">
@@ -102,7 +81,6 @@ class PartnerPage extends React.Component {
                         </div>
                       </div>
                     </Form>
-                    </LazyLoadComponent>
                   </div>
               </Col>
             </Row>
